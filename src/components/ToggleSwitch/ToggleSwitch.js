b50cc1c596175fd61591b52ee5eee29d8aa3af9b
Your Name <your_email@example.com>
2024-09-26 18:03:09 +0530

    latest

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,21 +1,22 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 import "./ToggleSwitch.scss";
 
 const ToggleSwitch = () => {
-  const {isDark} = useContext(StyleContext);
-  const [isChecked, setChecked] = useState(isDark);
+  const { isDark } = useContext(StyleContext);
+  // Set initial state to !isDark for the switch to be off by default
+  const [isChecked, setChecked] = useState(!isDark);
   const styleContext = useContext(StyleContext);
 
   return (
     <label className="switch">
       <input
         type="checkbox"
-        checked={isDark}
+        checked={isChecked} // Bind checked state to isChecked
         onChange={() => {
           styleContext.changeTheme();
-          setChecked(!isChecked);
+          setChecked(!isChecked); // Toggle the checked state
         }}
       />
       <span className="slider round">
@@ -24,4 +25,5 @@ const ToggleSwitch = () => {
     </label>
   );
 };
+
 export default ToggleSwitch;
